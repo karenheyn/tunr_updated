ee8b5fa57c26d1e85b50f7f7f73aba64b06510cd
karenheyn <karenheyn@gmail.com>
2019-12-04 17:10:23 -0500

    almost done with part 1

--- tunr/urls.py
+++ tunr/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('', views.artist_list, name='artist_list'),
-    path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
-    path('artists/new', views.artist_create, name='artist_create'),
-    path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
-    path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete')
+    path('artists', views.ArtistList.as_view(), name='artist_list'),
+    path('artists/<int:pk>', views.ArtistDetail.as_view(), name='artist_detail'),
+    path('song', views.SongList.as_view(), name='song_list'),
+    path('song/<int:pk>', views.SongDetail.as_view(), name='song_detail')
 ]
+
+
+# urlpatterns = [
+#     path('', views.artist_list, name='artist_list'),
+#     path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
+#     path('artists/new', views.artist_create, name='artist_create'),
+#     path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
+#     path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
+#     path('songs/', views.song_list, name='song_list'),
+#     path('songs/<int:pk>', views.song_detail, name='song_detail')
+# ]
 
 # const express = require('express')
 # const router = express.Router()
--- tunr/views.py
+++ tunr/views.py
@@ -1,38 +1,68 @@
 from django.shortcuts import render, redirect
-
+from rest_framework import generics
+from .serializers import ArtistSerializer, SongSerializer
 from .models import Artist, Song
 from .forms import ArtistForm
 
 # Create your views here.
-def artist_list(request):
-    artists = Artist.objects.all()
-    return render(request, 'tunr/artist_list.html', { 'artists': artists })
 
-def artist_detail(request, pk):
-    artist = Artist.objects.get(id=pk)
-    return render(request, 'tunr/artist_detail.html', { 'artist': artist })
+class ArtistList(generics.ListCreateAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
 
-def artist_create(request):
-    if request.method == 'POST':
-        form = ArtistForm(request.POST)
-        if form.is_valid():
-            artist = form.save()
-            return redirect('artist_detail', pk=artist.pk)
-    else:
-        form = ArtistForm()
-    return render(request, 'tunr/artist_form.html', { 'form' : form })
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
 
-def artist_edit(request, pk):
-    artist = Artist.objects.get(id=pk)
-    if request.method == 'POST':
-        form = ArtistForm(request.POST, instance=artist)
-        if form.is_valid():
-            artist = form.save()
-            return redirect('artist_detail', pk=artist.pk)
-    else:
-        form = ArtistForm(instance=artist)
-    return render(request, 'tunr/artist_form.html', { 'form': form })
+class SongList(generics.ListCreateAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
 
-def artist_delete(request, pk):
-    Artist.objects.get(id=pk).delete()
-    return redirect('artist_list')
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+
+
+
+
+
+# def artist_list(request):
+#     artists = Artist.objects.all()
+#     return render(request, 'tunr/artist_list.html', { 'artists': artists })
+
+# def artist_detail(request, pk):
+#     artist = Artist.objects.get(id=pk)
+#     return render(request, 'tunr/artist_detail.html', { 'artist': artist })
+
+# def artist_create(request):
+#     if request.method == 'POST':
+#         form = ArtistForm(request.POST)
+#         if form.is_valid():
+#             artist = form.save()
+#             return redirect('artist_detail', pk=artist.pk)
+#     else:
+#         form = ArtistForm()
+#     return render(request, 'tunr/artist_form.html', { 'form' : form })
+
+# def artist_edit(request, pk):
+#     artist = Artist.objects.get(id=pk)
+#     if request.method == 'POST':
+#         form = ArtistForm(request.POST, instance=artist)
+#         if form.is_valid():
+#             artist = form.save()
+#             return redirect('artist_detail', pk=artist.pk)
+#     else:
+#         form = ArtistForm(instance=artist)
+#     return render(request, 'tunr/artist_form.html', { 'form': form })
+
+# def artist_delete(request, pk):
+#     Artist.objects.get(id=pk).delete()
+#     return redirect('artist_list')
+
+# def song_list(request):
+#     songs = Song.objects.all()
+#     return render(request, 'tunr/song_list.html', {'songs': songs})
+
+# def song_detail(request, pk):
+#     song = Song.objects.get(id=pk)
+#     return render(request, 'tunr/song_detail.html', {'song': song})
